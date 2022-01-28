ac973cc7493e2a3de810c3f2a299891db4337e06
andrethigol <andrethigol@gmail.com>
2022-01-27 23:23:13 -0300

    Envio de teste

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+<main>
+  <MenuNovo />
+  <SliderNovo />
+<div>
+  <h1>teste</h1>
+  <h2>teste dois</h2>
+</div>
+  <FormUm />
+</main>
+</template>
+
+<script>
+import MenuNovo from '../components/menus/MenuNovo'
+
+
+export default{
+  components: {
+    MenuNovo
+}
+}
+</script>
